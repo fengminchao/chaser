d933f167f39ac66b497b023c72c2480b91ba8f13
fengminchao <360322537@qq.com>
2016-11-25 23:45:03 +0800

    searchview complete

--- app/src/main/java/com/muxistudio/chaser/ui/search/SearchActivity.java
+++ app/src/main/java/com/muxistudio/chaser/ui/search/SearchActivity.java
@@ -6,14 +6,16 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.view.MenuItemCompat;
-import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.SearchEvent;
-import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.SearchView;
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import com.miguelcatalan.materialsearchview.MaterialSearchView;
 import com.muxistudio.chaser.R;
 import com.muxistudio.chaser.ui.ToolbarActivity;
 
@@ -24,8 +26,8 @@ import com.muxistudio.chaser.ui.ToolbarActivity;
 public class SearchActivity extends ToolbarActivity {
 
   @BindView(R.id.toolbar) Toolbar mToolbar;
-  private MenuItem searchItem;
-  private SearchView mSearchView;
+  @BindView(R.id.search_view) MaterialSearchView mSearchView;
+  @BindView(R.id.toolbar_container) FrameLayout mToolbarContainer;
 
   public static void start(Context context) {
     Intent starter = new Intent(context, SearchActivity.class);
@@ -37,55 +39,74 @@ public class SearchActivity extends ToolbarActivity {
     setContentView(R.layout.activity_search);
     ButterKnife.bind(this);
     initToolbar(mToolbar);
-    initView();
+    //initView();
   }
 
   private void initView() {
-    SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
-    mSearchView = new SearchView(getSupportActionBar().getThemedContext());
-    mSearchView.setSubmitButtonEnabled(true);
-    mSearchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
-    mSearchView.setIconifiedByDefault(true);
-    mSearchView.setMaxWidth(1000);
-    mSearchView.setOnSearchClickListener(new View.OnClickListener() {
-      @Override public void onClick(View view) {
-        showSearch(true);
+    mSearchView.setAnimationDuration(0);
+    mSearchView.setOnQueryTextListener(new MaterialSearchView.OnQueryTextListener() {
+      @Override
+      public boolean onQueryTextSubmit(String query) {
+        //Do some magic
+        return false;
+      }
+
+      @Override
+      public boolean onQueryTextChange(String newText) {
+        //Do some magic
+        return false;
       }
     });
-    SearchView.SearchAutoComplete autoComplete =
-        (SearchView.SearchAutoComplete) mSearchView.findViewById(
-            android.support.v7.appcompat.R.id.search_src_text);
-    autoComplete.setOnFocusChangeListener(new View.OnFocusChangeListener() {
-      @Override public void onFocusChange(View view, boolean b) {
-        if (!b) {
-          showSearch(false);
-        }
+
+    mSearchView.setOnSearchViewListener(new MaterialSearchView.SearchViewListener() {
+      @Override
+      public void onSearchViewShown() {
+        //Do some magic
+      }
+
+      @Override
+      public void onSearchViewClosed() {
+        //Do some magic
       }
     });
   }
 
   @Override public boolean onCreateOptionsMenu(Menu menu) {
-    searchItem = menu.add(android.R.string.search_go);
-    searchItem.setIcon(R.drawable.ic_search_white_24dp);
-    MenuItemCompat.setActionView(searchItem, mSearchView);
-    MenuItemCompat.setShowAsAction(searchItem,
-        MenuItemCompat.SHOW_AS_ACTION_ALWAYS | MenuItemCompat.SHOW_AS_ACTION_COLLAPSE_ACTION_VIEW);
 
+    getMenuInflater().inflate(R.menu.search,menu);
+    MenuItem item = menu.findItem(R.id.action_search);
+    mSearchView.setMenuItem(item);
     return true;
+    //searchItem = menu.add(android.R.string.search_go);
+    //searchItem.setIcon(R.drawable.ic_search_white_24dp);
+    //MenuItemCompat.setActionView(searchItem, mSearchView);
+    //MenuItemCompat.setShowAsAction(searchItem,
+    //    MenuItemCompat.SHOW_AS_ACTION_ALWAYS | MenuItemCompat.SHOW_AS_ACTION_COLLAPSE_ACTION_VIEW);
+
+    //MenuInflater menuInflater = getMenuInflater();
+    //menuInflater.inflate(R.menu.search, menu);
+    //
+    //MenuItem searchItem = menu.findItem(R.id.search);
+    //mSearchView = (SearchView) searchItem.getActionView();
+    //mSearchView.setQueryHint("fff");
+    //
+    //SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+    //mSearchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+    //return true;
   }
 
   @Override public boolean onSearchRequested(SearchEvent searchEvent) {
-    showSearch(true);
+    //showSearch(true);
     return false;
   }
 
-  private void showSearch(boolean b) {
-    if (b) {
-      MenuItemCompat.expandActionView(searchItem);
-    } else {
-      MenuItemCompat.collapseActionView(searchItem);
-    }
-  }
+  //private void showSearch(boolean b) {
+  //  if (b) {
+  //    MenuItemCompat.expandActionView(searchItem);
+  //  } else {
+  //    MenuItemCompat.collapseActionView(searchItem);
+  //  }
+  //}
 
   @Override public boolean onOptionsItemSelected(MenuItem item) {
     return super.onOptionsItemSelected(item);
